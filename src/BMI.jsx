d2f1cb9b30299f7bc360193f2e0b5cb7e543f806
shireen <shireenk705@gmail.com>
2024-07-06 19:40:29 +0500

    BMI done.

--- src/BMI.jsx
+++ src/BMI.jsx
@@ -4,16 +4,22 @@ import "./BMIScroll.css";
 import bmiCover from "./Assets/bmiCover.jpg";
 
 export default function BMI() {
-  //   const height = document.getElementById("height");
-  //     const weight = document.getElementById("weight");
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
+  const [units, setUnits] = useState("metric");
   const [bmiResult, setBmiResult] = useState(null);
   const calculateBMI = () => {
     if (height && weight) {
-      const heightInMeters = parseFloat(height);
-      const weightInKg = parseFloat(weight);
-      const bmi = weightInKg / (heightInMeters * heightInMeters);
+      let bmi;
+      if (units === "metric") {
+        const heightInMeters = parseFloat(height) / 100;
+        const weightInKg = parseFloat(weight);
+        bmi = weightInKg / (heightInMeters * heightInMeters);
+      } else {
+        const heightInInches = parseFloat(height);
+        const weightInPounds = parseFloat(weight);
+        bmi = (weightInPounds / (heightInInches * heightInInches)) * 703;
+      }
       setBmiResult(bmi.toFixed(2));
       console.log("BMI Calculated:", bmi);
     }
@@ -25,36 +31,49 @@ export default function BMI() {
         backgroundImage: `url(${bmiCover})`,
       }}
     >
-      <h2 className="text-4xl">Lets get started!</h2>
-      <h3 className="text-xl">By calculating your BMI</h3>
+      <div className="">
+        <h2 className="text-4xl py-8">Lets get started!</h2>
+        <h3 className="text-xl">By calculating your BMI</h3>
+      </div>
       <div className="bg-sky-950 bg-opacity-50">
-        <form className="flex flex-col items-center">
+        <form className="flex flex-col items-center py-2">
           <label htmlFor="height">Enter your Height</label>
           <input
-            className="py-2 px-8 my-4 text-black rounded "
+            className="py-2 px-8 my-4 bg-sky-950 rounded-md "
             id="height"
             type="number"
-            placeholder="1.8 meters"
+            placeholder={units === "metric" ? "180 cm" : "70 inches"}
             step={0.01}
             value={height}
             onChange={(e) => setHeight(e.target.value)}
           />
           <label htmlFor="weight">Enter your Weight</label>
           <input
-            className="py-2 px-8 my-4 text-black rounded "
+            className="py-2 px-8 my-4 bg-sky-950 rounded-md "
             id="weight"
             type="number"
-            placeholder="70 kg"
+            placeholder={units === "metric" ? "70 kg" : "150 lbs"}
             step={0.01}
             value={weight}
             onChange={(e) => setWeight(e.target.value)}
           />
-          <input
+          <select
+            className="bg-sky-950 px-2 rounded"
+            name="units"
+            id="units"
+            value={units}
+            onChange={(e) => setUnits(e.target.value)}
+          >
+            <option value="metric">Metric</option>
+            <option value="imperial">Imperial</option>
+          </select>
+          <button
             className="py-2 px-8 my-4 rounded bg-sky-900 cursor-pointer transition ease-in-out duration-300 hover:scale-105 hover:bg-sky-800"
             type="button"
-            value="Calculate"
             onClick={calculateBMI}
-          />
+          >
+            Calculate
+          </button>
         </form>
         {bmiResult && (
           <div className="mt-4 text-white italic">
